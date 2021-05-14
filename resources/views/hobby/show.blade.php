b4d9aac527b50347f16edc1630ca95669ceaf8ab
KevinSoulh <kevin08@outlook.com.br>
2021-05-14 15:43:59 -0300

    Hobby details + show.blade created

--- resources/views/hobby/show.blade.php
+++ resources/views/hobby/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.aap')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -7,9 +7,11 @@
             <div class="card">
                 <div class="card-header">Hobby Details</div>
                 <div class="card-body">
-
+                    <p>{{ $hobby->name }}</p>
+                    <p>{{ $hobby->description }}</p>
                 </div>
             </div>
+            <a class="btn btn-primary mt-2" href="/hobby">Back</a>
         </div>
     </div>
 </div>
