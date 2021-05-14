7f804e3ab21f08b1034e7fc95e720d2d8492f599
KevinSoulh <kevin08@outlook.com.br>
2021-05-14 10:36:19 -0300

    UserController + Database tables

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-11">
+            <div class="card">
+                <div class="card-header"></div>
+                <div class="card-body">
+                    <ul class="form-group">
+                        <li class="list-group-item">
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
